84b011d2607449244239746699d2a742cdea2632
andjelamrdak <anja.mrdak2@gmail.com>
2022-02-02 13:23:01 +0100

    Druga stranica contact, reusable komponenta contactform, prenos funkcije preko props

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './components/Main'
-
+import Contact from './components/Contact'
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="/home" element={<Main />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router >
     </div>
